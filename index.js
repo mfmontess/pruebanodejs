400649a9b5dbe86a740a860bd4432b8bc5d11574
mfmontess <mmontess@ucentral.edu.co>
2019-05-08 16:05:43 -0500

    uso de jquery

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var http = require('http'),
     fs = require('fs'),
     mustache = require('mustache');
 	
-
-//var products = require('./fixtures/productsFixture.js');
-//var productSummaryTemplate = fs.readFileSync('component_templates/product_summary.html','utf-8');
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type':'text/html'});
 	promisedIdentity('pages_templates/product_list.html')
@@ -25,33 +22,10 @@ function promisedIdentity(x) {
 		global.window = window;
 		global.document = document;
 		const $ = global.jQuery = require( 'jquery' );
-				
-		//window.document.querySelector("#titulo").innerHTML = "Prueba5";
-		$("#titulo").innerHTML = "Prueba5";
-		$("#user").val("Michael");
-		console.log(window.document.querySelector("#titulo").innerHTML);
-		resolve("<!DOCTYPE html>\n" + window.document.querySelector("html").outerHTML)
+		
+		$("#titulo").text('Prueba5');
+		$("#user").attr('value','Fabian');
+		resolve("<!DOCTYPE html>\n" + $("html").html())
     }, 0)
   })
 }
-
-/*http.createServer(function(request,response) {
-		var camino = 'pages_templates/product_list.html';
-		var content = fs.readFileSync(camino, 'utf8');
-		const { JSDOM } = require( 'jsdom' );
-		const jsdom = new JSDOM( content );
-		const { window } = jsdom;
-		const { document } = window;
-		global.window = window;
-		global.document = document;
-		const $ = global.jQuery = require( 'jquery' );
-				
-		window.document.querySelector("#titulo").innerHTML = "Prueba4";
-		console.log($("#user").val());
-		$("#user").val("Michael");
-		//console.log(window.document.querySelector("#titulo").innerHTML);
-		
-		response.writeHead(200, { 'Content-Type': 'text/html' });
-		response.write("<!DOCTYPE html>\n" + window.document.querySelector("html").outerHTML);
-		response.end();
-    }).listen(8078);*/
